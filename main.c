d842257f23136045bcd3c9977ae286a840858313
Lorenzo <lorenzo.romagnoni@tutored.it>
2020-07-20 21:43:20 +0200

    Built infrastructure

--- main.c
+++ main.c
@@ -1,19 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define MAX_LINE_LENGTH 1024
 
-void getCommand(char *cmd){
+typedef struct command {
+    int arg1;
+    int arg2;
+    char type;
+} Command;
+
+void printCmd(Command cmd){
+    printf("cmd.arg1 = %d\n", cmd.arg1);
+    printf("cmd.arg2 = %d\n", cmd.arg2);
+    printf("cmd.type = %c\n", cmd.type);
+}
+
+Command getCommand(char *cmd){
     printf("Insert command\n");
     fgets(cmd, MAX_LINE_LENGTH, stdin);
-}
-int main() {
-
-    char cmdRaw[MAX_LINE_LENGTH];
-    for (int i = 0; i < 3; ++i) {
-        getCommand(cmdRaw);
-        
-        printf("I have read: %s\n", cmdRaw);
+    if(strcmp(cmd, "q\n")==0) return (Command){.arg1=-1, .arg2=-1, .type='q'};
+    char *ptr;
+    int arg1 = (int) strtol(cmd, &ptr, 10);
+    int arg2 = -1;
+    char type;
+    if(*ptr=='u'||*ptr=='r') {
+        type = *ptr;
+    }else{
+        arg2 = (int) strtol(ptr+1, &ptr, 10);
+        type = *ptr;
     }
+    return (Command){.arg1 = arg1, .arg2 = arg2, .type = type};
+}
 
+void execute(Command cmd){
+    printCmd(cmd);
+    switch (cmd.type) {
+        case 'c':
+            break;
+        case 'd':
+            break;
+        case 'u':
+            break;
+        case 'r':
+            break;
+        case 'p':
+            break;
+        case 'q':
+            exit(1);
+    }
+}
+
+int main() {
+    char cmdRaw[MAX_LINE_LENGTH];
+    //printf("%lu",sizeof(cmd.arg1));
+    //do{
+        execute(getCommand(cmdRaw));
+    //}while(1);
     return 0;
 }
 
